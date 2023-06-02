d104a29781a28791e703b4f82320d667867c0bb5
Manix <admin@manix.info>
2023-06-02 11:57:44 +0300

    init

---
+++ js/lib/auth.js
@@ -0,0 +1,45 @@
+import TosheUser from "../../../app/js/lib/user.js";
+import luri from "./luri.js";
+
+let u = null;
+
+class AuthManager {
+
+  login(user) {
+    if (!user) {
+      return;
+    }
+
+    u = user;
+
+    luri.emit("LoggedIn", user);
+  }
+
+  logout() {
+    let x = u;
+
+    u = null;
+    
+    luri.emit("LoggedOut", x);
+  }
+
+  logged() {
+    return !!u;
+  }
+
+  user() {
+    return u;
+  }
+
+  throwable = Error
+
+  require(message) {
+    if (!this.logged()) {
+      throw new this.throwable(message);
+    }
+  }
+}
+
+let auth = new AuthManager;
+
+export default auth;
